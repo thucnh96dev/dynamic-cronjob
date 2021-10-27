1b7e386073e736347d3c6d8357008301f50fb76d
Trung Nguyễn <thucnh96.dev@gmail.com>
2021-10-27 10:15:57 +0700

    init commit

---
+++ src/main/java/com/thucnh/cronjob/DynamicCronjobApplication.java
@@ -0,0 +1,13 @@
+package com.thucnh.cronjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicCronjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicCronjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thucnh/cronjob/DynamicCronjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thucnh.cronjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicCronjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
